75b4fe3f4dd5012df1c6cbf0b1f7dfc97f5f4047
Jordan Burke <jordan.burke@gmail.com>
2020-11-06 15:36:28 -0500

    Added an isEmpty to the DataResult.

--- src/main/scala/com/sapienapps/http4s/package.scala
+++ src/main/scala/com/sapienapps/http4s/package.scala
@@ -2,10 +2,16 @@ package com.sapienapps
 
 package object http4s {
 
-  sealed trait DataResult[T]
+  sealed trait DataResult[T] {
+    def isEmpty: Boolean
+  }
 
-  case class ItrResult[T](list: Iterable[T]) extends DataResult[T]
+  case class ItrResult[T](list: Iterable[T]) extends DataResult[T] {
+    def isEmpty: Boolean = list.isEmpty
+  }
 
-  case class CountResult[T](value: Int) extends DataResult[T]
+  case class CountResult[T](value: Int) extends DataResult[T] {
+    def isEmpty: Boolean = value == 0
+  }
 
 }
